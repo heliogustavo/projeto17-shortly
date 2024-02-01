8dcc2c1a814ed3d3db38b9a47ad416edde851b0a
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2024-02-01 16:27:54 -0400

    Cadastro(sign-up) implementado

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,29 @@
-export async function signUp(req,res){
-    res.send("signup")
+import { db } from "../database/db.connection.js"
+import bcrypt from "bcrypt"
+
+export async function signUp(req, res) {
+    const { name, email, password } = req.body
+
+    try {
+        const user = await db.query(`SELECT * FROM users WHERE email=$1;`, [email])
+        //status 409 é de conflito
+        if (user.rowCount !== 0) return res.status(409).send({ message: "Este email já foi cadastrado!" })
+
+        const hash = bcrypt.hashSync(password, 10)
+
+        await db.query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3);`,
+            [name, email, hash]
+        )
+
+        res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+    res.sendStatus(201)
 }
 
-export async function signIn(req,res){
+export async function signIn(req, res) {
     res.send("signin")
 }
